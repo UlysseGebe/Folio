a0cafce80ff220968fc804c75317fda0b3d61826
UlysseGebe <Uly35534e31>
2019-05-07 17:30:41 +0200

    contact

--- script/main.js
+++ script/main.js
@@ -1,13 +1,30 @@
 const $header = document.querySelector('header')
+const $logo = $header.querySelector('h1')
 const $pres = document.querySelector('.presentation')
-let limit = $pres.offsetHeight / 1.05
 
-window.addEventListener('scroll', () => {
-    let position = $pres.offsetHeight - window.pageYOffset
-    if (position < limit) {
-        $header.style.opacity = 1
-    }
-    else {
-        $header.style.opacity = 0
-    }
-})
+if (window.location.href.indexOf("contact") != -1) {
+    $header.style.backgroundColor = '#ffffff'
+    $header.style.borderBottom = '5px solid #f2f2f2'
+    $logo.style.display = 'block'
+    $logo.style.opacity = '1'
+}
+else {
+    let limit = $pres.offsetHeight / 1.05
+    window.addEventListener('scroll', () => {
+        let position = $pres.offsetHeight - window.pageYOffset
+        if (position < limit) {
+            $header.style.backgroundColor = '#ffffff'
+            $header.style.borderBottom = '5px solid #f2f2f2'
+            $logo.style.display = 'block'
+            $logo.style.opacity = '1'
+        }
+        else if (position > limit) {
+            $header.style.backgroundColor = 'rgba(255, 255, 255, 0)'
+            $header.style.borderBottom = 'none'
+            $logo.style.opacity = '0'
+            $logo.style.display = 'none'
+
+        }
+    })
+}
+
